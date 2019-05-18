2f1ad88ae3dcdbb095815dd2a311ed9a569866db
Mohit Mair <mohit.mair@gmail.com>
2019-05-18 23:51:52 +0100

    created basic h2 program

--- src/main/java/H2.java
+++ src/main/java/H2.java
@@ -1,10 +1,59 @@
+
+
+import java.sql.*;
+
 public class H2 {
+
 
     public static void main(String[] args) {
 
+        try {
 
+            //Create a database in memory and open connection
+            Connection h2DBConnection = getDBConnection();
+
+            //Initialize Statement
+            Statement h2BDStatement = h2DBConnection.createStatement();
+
+            //Create a table
+            h2BDStatement.executeUpdate("CREATE TABLE CUSTOMERS (ID INT NOT NULL, NAME VARCHAR (20) NOT NULL)");
+
+            //Insert some records
+            h2BDStatement.executeUpdate("insert into CUSTOMERS values(1,'MOHIT')");
+            h2BDStatement.executeUpdate("insert into CUSTOMERS values(1,'MOHIT')");
+            h2BDStatement.executeUpdate("insert into CUSTOMERS values(1,'MOHIT')");
+
+            //Get recordset
+            ResultSet h2DBResultset=h2BDStatement.executeQuery("Select count(*) as COUNT from CUSTOMERS");
+
+            //Print values
+            while(h2DBResultset.next()){
+                System.out.println(h2DBResultset.getInt(1));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
+    //Make a connection to the H2 Database
+    private static Connection getDBConnection() {
+
+        Connection conn;
+
+        try {
+            Class.forName("org.h2.Driver").newInstance();
+            conn = DriverManager.getConnection("jdbc:h2:mem:test");
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            conn = null;
+
+        }
+
+        return conn;
+    }
 
 }
